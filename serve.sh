c3fbc756432c60e1acc472ab081c5745f2d89a4f
cdcdopenclaw <cdcdopenclaw@gmail.com>
2026-05-03 09:26:15 +0800

    chore: remove serve.sh (replaced by build.mjs standalone HTML)

--- serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 启动游戏开发服务器
-# 打开 http://localhost:8765/src/ui/index.html
-cd "$(dirname "$0")"
-echo "🃏 扑克牌对战 - 开发服务器"
-echo "→ http://localhost:8765/src/ui/index.html"
-echo "按 Ctrl+C 停止"
-python3 -m http.server 8765
